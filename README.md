b6d6bafb0f433267d4e588e7efeefe424671d93f
obi <obiorah.edeh@stemcell.com>
2024-07-15 00:17:59 -0400

    ci: test

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-# Welcome to your CDK TypeScript project
+# Welcome to CDK TypeScript project by obiorah for BMO
+
+# Pre-requisite for set up
+1. Install AWS CLI
+2. Have an AWS Account
+3. Install Nodejs (16)
+4. Install TypeScript 3.8 or later `npm -g install typescript`
+5. Install AWS CDK CLI `npm install -g aws-cdk`
+6. Configure AWS security credentials for the CDK CLI.
+
+# Usage
+1. Clone this repository
+2. Modify `cdk.context.json` with your environments and account details on stage. You can remove or add more stages.
+3. Ensure AWS security credential is configured
+4. Boostrap all accounts provided in step 2, simply by running the boostrap.sh script `sh bootstrap.sh`. [One time setup]
+5. Deploy cdk codepipeline. Run `npx cdk deploy ObiCdkStack`. [One time setup]
+6. Go into the dev AWS account to confirm that the pipeline has been created.
+7. Modify/Update your environment/AWS account variables in the file `constants/constants.ts`
+7. Modify or add new stacks/stages to the pipeline in the folder `lib/`
+
+# 3-tier Architecture
+
+# Codepipeline Architecture
+
+
+
+
+
+
 
 This is a blank project for CDK development with TypeScript.
 
@@ -12,3 +40,5 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npx cdk deploy`  deploy this stack to your default AWS account/region
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
+
+
--- lib/compute-stack.ts
+++ lib/compute-stack.ts
@@ -1,7 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import { Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
 import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
@@ -24,23 +22,11 @@ export class ComputeStack extends cdk.Stack {
     readonly envName: string
     constructor(scope: Construct, id: string, props?: ComputeStackProps) {
         super(scope, id, props);
-        
 
-
-        /* ****************************************************** */
-        /* *************** Env/Branch specific suffix *********** */
-        /* ****************************************************** */
-        
-        /* istanbul ignore next */
-        console.log(this.account)
-        console.log(this.region)
         const config = EnvironmentConfigs[this.account];
-        console.log(config.appSubnets[this.region][0])
         const vpcId = config.vpcId[this.region]
         const subnetId1 = config.appSubnets[this.region][0]
         const subnetId2 = config.appSubnets[this.region][1]
-        const subnetLb1 = config.lbSubnets[this.region][0]
-        const subnetLb2 = config.lbSubnets[this.region][1]
         const route_53 = ROUTE_53
         const DOMAIN_NAMES = config.domainName
         // const zoneName = DOMAIN_NAMES
@@ -123,25 +109,22 @@ export class ComputeStack extends cdk.Stack {
         // })
         // globalAlias.node.addDependency(ARecord)
 
-        // Build dockerfile and publish to ecr. Remove comment if you want build
-        const dockerImageAsset = new ecrasset.DockerImageAsset(this, 'appDockerImage', {
-            directory: '.',
-            target: 'bundle',
-        })
-
-          
+        // Build dockerfile and publish to ecr. Remove comment if you want build from app source code
+        // const dockerImageAsset = new ecrasset.DockerImageAsset(this, 'appDockerImage', {
+        //     directory: '.',
+        //     target: 'bundle',
+        // })
 
         // Use ecs pattern loadbalanced fargate
         const loadBalancedFargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'Service', {
             cluster,
-            
             memoryLimitMiB: 1024,
             desiredCount: 2,
             cpu: 512,
             taskImageOptions: {
-                // image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")
                 // use this dockerImage Asset to do build
-                image: ecs.ContainerImage.fromDockerImageAsset(dockerImageAsset),
+                // image: ecs.ContainerImage.fromDockerImageAsset(dockerImageAsset),
+                image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")
             },
             taskSubnets: {
             subnets: subnets,
